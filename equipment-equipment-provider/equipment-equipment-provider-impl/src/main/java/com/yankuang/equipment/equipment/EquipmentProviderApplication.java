cbb0518d2e801cfb86fd37638e44c546447b9474
liyp <13864241798@139.com>
2018-08-04 10:56:03 +0800

    init equipment-equipment

---
+++ equipment-equipment-provider/equipment-equipment-provider-impl/src/main/java/com/yankuang/equipment/equipment/EquipmentProviderApplication.java
@@ -0,0 +1,18 @@
+package com.yankuang.equipment.equipment;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication(scanBasePackages = "com.yankuang.equipment.equipment")
+@MapperScan("com.yankuang.equipment.equipment.mapper")
+public class EquipmentProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(EquipmentProviderApplication.class);
+        application.run(args);
+    }
+}
---
+++ equipment-equipment-provider/equipment-equipment-provider-impl/src/main/java/com/yankuang/equipment/equipment/service/impl/SbTypeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.yankuang.equipment.equipment.service.impl;
+
+import com.yankuang.equipment.equipment.mapper.SbTypeMapper;
+import com.yankuang.equipment.equipment.model.SbType;
+import com.yankuang.equipment.equipment.service.SbTypeService;
+import io.terminus.boot.rpc.common.annotation.RpcProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RpcProvider(version = "0.0.1")
+public class SbTypeServiceImpl implements SbTypeService {
+
+    @Autowired
+    private SbTypeMapper sbTypeMapper;
+
+    public List<SbType> selectAllSbTypes() {
+        return sbTypeMapper.selectAllSbTypes();
+    }
+
+    public void insertSbType(SbType record){
+        sbTypeMapper.insert(record);
+    }
+}
