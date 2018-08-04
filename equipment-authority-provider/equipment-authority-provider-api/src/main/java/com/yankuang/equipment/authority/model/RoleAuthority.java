7701ffbafbfe8813bfbc394e6311413e2325f15b
14011 <1401165972@qq.com>
2018-08-04 13:27:26 +0800

    添加关联表实体类

---
+++ equipment-authority-provider/equipment-authority-provider-api/src/main/java/com/yankuang/equipment/authority/model/RoleAuthority.java
@@ -0,0 +1,96 @@
+package com.yankuang.equipment.authority.model;
+
+import javax.persistence.Table;
+
+@Table(name = "el_role_authority_mapping")
+public class RoleAuthority {
+    private Integer id;
+
+    private String roleCode;
+
+    private String authorityCode;
+
+    private String createBy;
+
+    private Long updateAt;
+
+    private String updateBy;
+
+    private Long createAt;
+
+    private Long version;
+
+    private Byte status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode == null ? null : roleCode.trim();
+    }
+
+    public String getAuthorityCode() {
+        return authorityCode;
+    }
+
+    public void setAuthorityCode(String authorityCode) {
+        this.authorityCode = authorityCode == null ? null : authorityCode.trim();
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Long getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Long updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy == null ? null : updateBy.trim();
+    }
+
+    public Long getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Long createAt) {
+        this.createAt = createAt;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+}
