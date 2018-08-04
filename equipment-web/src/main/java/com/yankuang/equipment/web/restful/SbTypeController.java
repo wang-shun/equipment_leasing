b8bd3bbb883b73b6dab4aa0e80f102bb4157ad58
liyp <13864241798@139.com>
2018-08-04 11:03:56 +0800

    init equipment-controller

--- equipment-web/src/main/java/com/yankuang/equipment/web/restful/SbTypeController.java
+++ equipment-web/src/main/java/com/yankuang/equipment/web/restful/SbTypeController.java
@@ -1,4 +1,36 @@
 package com.yankuang.equipment.web.restful;
 
+import com.yankuang.equipment.equipment.model.SbType;
+import com.yankuang.equipment.equipment.service.SbTypeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.terminus.boot.rpc.common.annotation.RpcConsumer;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api
+@CrossOrigin(maxAge = 3600)
+@RestController
+@RequestMapping("/equipment/type")
 public class SbTypeController {
+
+    @RpcConsumer(version = "0.0.1",check = "false")
+    private SbTypeService sbTypeService;
+
+    @ApiOperation("select all shebei types")
+    @RequestMapping(value = "/types")
+    public List<SbType> selectAllSbTypes(){
+        return sbTypeService.selectAllSbTypes();
+    }
+
+    @ApiOperation("insert into shebei record")
+    @RequestMapping(value = "/insert")
+    public void insertSbType(SbType sbType){
+        sbTypeService.insertSbType(sbType);
+    }
 }
