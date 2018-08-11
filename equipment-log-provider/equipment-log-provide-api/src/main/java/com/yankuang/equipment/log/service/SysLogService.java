23c5eec46c5b60b5fa4d83d75a6ccbb6fe435f45
xianhaihui <1320115862@qq.com>
2018-08-11 15:03:08 +0800

    log日志管理

---
+++ equipment-log-provider/equipment-log-provide-api/src/main/java/com/yankuang/equipment/log/service/SysLogService.java
@@ -0,0 +1,13 @@
+package com.yankuang.equipment.log.service;
+
+
+import com.yankuang.equipment.log.model.SysLog;
+
+/**
+ * @author xhh
+ * @date 2018/8/10 14:05
+ */
+public interface SysLogService {
+
+    boolean add(SysLog syslog);
+}
