9398a7bb738e2427f1e3031136e54cb5948b9307
14011 <1401165972@qq.com>
2018-08-15 15:43:31 +0800

    组织管理模块接口调整

--- equipment-web/src/main/java/com/yankuang/equipment/web/restful/OrganizationController.java
+++ equipment-web/src/main/java/com/yankuang/equipment/web/restful/OrganizationController.java
@@ -11,7 +11,9 @@ import io.terminus.boot.rpc.common.annotation.RpcConsumer;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author boms
@@ -23,211 +25,119 @@ import java.util.List;
 public class OrganizationController {
 
     @RpcConsumer
-    private OrganizationService organizationService;
+    OrganizationService organizationService;
 
     /**
-     * @method 通过id查询
      * @param id
      * @return
+     * @method 通过id查询
      */
     @GetMapping("/{id}")
-    CommonResponse getById(@PathVariable Long id) {
-        return CommonResponse.ok(organizationService.getById(id));
+    public CommonResponse findById(@PathVariable Long id) {
+        return CommonResponse.ok(organizationService.findById(id));
     }
 
     /**
-     * @method 添加
      * @param organization
      * @return
+     * @method 添加
      */
-    @PostMapping("/add")
-    public CommonResponse add(@RequestBody Organization organization){
-        if (organization.getName() == null || " ".equals(organization.getName())){
+    @PostMapping
+    public CommonResponse create(@RequestBody Organization organization) {
+        if (StringUtils.isEmpty(organization.getName())) {
             return CommonResponse.errorMsg("请填写组织名称");
         }
-
-        if (organizationService.getByName(organization.getName()) != null){
+        if (organizationService.findByName(organization.getName()) != null) {
             return CommonResponse.errorMsg("此组织名称已存在");
         }
+        if (StringUtils.isEmpty(organization.getpId())) {
+            return CommonResponse.errorMsg("父级pId不能为空");
+        }
 
-        int level = organization.getLevel() == null?0:organization.getLevel();
+        int level = organization.getLevel() == null ? 0 : organization.getLevel();
         organization.setLevel(level);
 
-        String pcode = (organization.getPcode() == null || " ".equals(organization.getPcode())) ?"1":organization.getPcode();
+        Long pId = (StringUtils.isEmpty(organization.getpId())) ? 0 : organization.getpId();
+        organization.setpId(pId);
+
+        String pcode = (organization.getPcode() == null || " ".equals(organization.getPcode())) ? "1" : organization.getPcode();
         organization.setPcode(pcode);
 
-        int sort = organization.getSorting() == null ?1:organization.getSorting();
+        int sort = organization.getSorting() == null ? 1 : organization.getSorting();
         organization.setSorting(sort);
 
-        Long version = (organization.getVersion() == null || organization.getVersion() == 0) ?1:organization.getVersion();
+        Long version = (organization.getVersion() == null || organization.getVersion() == 0) ? 1 : organization.getVersion();
         organization.setVersion(version);
 
         organization.setCode(UuidUtils.newUuid());
         organization.setCreateBy("admin");//TODO 由于用户功能暂未开发完，先写死，后期改
         organization.setUpdateBy("admin");
 
-        return CommonResponse.ok(organizationService.add(organization));
+        return CommonResponse.ok(organizationService.create(organization));
     }
 
     /**
-     * @method 更新
      * @param jsonString
      * @return
+     * @method 更新
      */
-    @PutMapping("/udt")
-    public CommonResponse update(@RequestBody String jsonString){
-        if (StringUtils.isEmpty(jsonString)){
+    @PutMapping
+    public CommonResponse update(@RequestBody String jsonString) {
+        if (StringUtils.isEmpty(jsonString)) {
             return CommonResponse.errorTokenMsg("参数不能为空");
         }
+        Organization organization = JsonUtils.jsonToPojo(jsonString, Organization.class);
 
-        Organization organization = JsonUtils.jsonToPojo(jsonString,Organization.class);
-
-        if (organization.getId() == null || organization.getId() == 0){
-            return CommonResponse.errorMsg("系统错误");
+        if (StringUtils.isEmpty(organization.getId())) {
+            return CommonResponse.errorMsg("id不能为空");
         }
-        if (organization.getPcode() == null || " ".equals(organization.getPcode())){
-            return CommonResponse.errorMsg("系统错误");
-        }
-
         return CommonResponse.ok(organizationService.update(organization));
     }
 
     /**
-     * @mehtod 删除
      * @param jsonString
      * @return
+     * @mehtod 根据id列表删除
+     * 入参是id列表，一个或者多个
      */
-    @DeleteMapping("/dels")
-    public CommonResponse delete(@RequestBody String jsonString){
-        if(com.yankuang.equipment.common.util.StringUtils.isEmpty(jsonString)){
+    @DeleteMapping
+    public CommonResponse delete(@RequestBody String jsonString) {
+        if (StringUtils.isEmpty(jsonString)) {
             return CommonResponse.errorTokenMsg("没有查询的id");
         }
-        List<Long> ids = JsonUtils.jsonToList(jsonString,Long.class);
-        for (Long id: ids){
-            boolean idB = organizationService.del(id);
-            if (idB == false){
+        List<Long> ids = JsonUtils.jsonToList(jsonString, Long.class);
+        for (Long id : ids) {
+            boolean idB = organizationService.delete(id);
+            if (idB == false) {
                 return CommonResponse.errorTokenMsg("删除失败");
             }
         }
-        return  CommonResponse.ok();
+        return CommonResponse.ok();
     }
 
+
     /**
-     * @method 通过名字查找
-     * @param name
      * @return
+     * @method 查询所有
+     */
+    @GetMapping(value = "/all")
+    public CommonResponse findAll() {
+        return CommonResponse.ok(organizationService.findAll());
+    }
+
+
+    /**
+     * @param page
+     * @param size
+     * @return
+     * @method 分页查询
      */
     @GetMapping
-    public CommonResponse getByName(@RequestParam String name){
-        if (name == null || " ".equals(name)){
-            return CommonResponse.errorMsg("组织名称不能为空");
-        }
-        return CommonResponse.ok(organizationService.getByName(name));
+    public CommonResponse findByPage(@RequestParam(value = "page", defaultValue = "1") int page,
+                                     @RequestParam(value = "size", defaultValue = "20") int size,
+                                     @RequestParam(value = "searchInput", defaultValue = "") String serachInput) {
+        Map orgMap = new HashMap();
+        return CommonResponse.ok(organizationService.findByPage(page, size, orgMap));
     }
 
-    /**
-     * @method 查询所有
-     * @return
-     */
-    @GetMapping(value = "/findAll")
-    public CommonResponse getAll( ){
-        return CommonResponse.ok(organizationService.getAll( ));
-    }
-
-
-    /**
-     * @method 分页查询
-     * @param page
-     * @param limit
-     * @return
-     */
-    @GetMapping("/findpage")
-    public CommonResponse getPage(@RequestParam int page,@RequestParam int limit){
-        Organization organization = new Organization();
-        return CommonResponse.ok(organizationService.findpage(page,limit,organization));
-    }
-
-    /**
-     * @method 查询所有名字
-     * @return
-     */
-
-    @GetMapping("/findName")
-    public CommonResponse getName(){
-        return CommonResponse.ok(organizationService.findName());
-    }
-
-    /**
-     * @method 公司管理添加功能
-     * @param pOrgsId
-     * @param OrgsName
-     * @return
-     */
-    @PostMapping("/orgsAdd")
-    public CommonResponse orgsAdd(@RequestParam String pOrgsId,@RequestParam String OrgsName){
-        if (pOrgsId == null || " ".equals(pOrgsId)){
-            return CommonResponse.errorTokenMsg("系统错误");
-        }
-
-        if (OrgsName == null || " ".equals(OrgsName)){
-            return CommonResponse.errorMsg("请填写组织名称");
-        }
-
-        if (organizationService.getByName(OrgsName) != null){
-            return CommonResponse.errorMsg("此组织名称已存在");
-        }
-
-        Organization organization = new Organization();
-
-        organization.setName(OrgsName);
-        organization.setPcode(pOrgsId);
-
-        int level = organization.getLevel() == null?0:organization.getLevel();
-        organization.setLevel(level);
-
-        String pcode = (organization.getPcode() == null || " ".equals(organization.getPcode())) ?"1":organization.getPcode();
-        organization.setPcode(pcode);
-
-        int sort = organization.getSorting() == null ?1:organization.getSorting();
-        organization.setSorting(sort);
-
-        Long version = (organization.getVersion() == null || organization.getVersion() == 0) ?1:organization.getVersion();
-        organization.setVersion(version);
-
-        organization.setCode(UuidUtils.newUuid());
-        organization.setCreateBy("admin");//TODO 由于用户功能暂未开发完，先写死，后期改
-        organization.setUpdateBy("admin");
-
-        return CommonResponse.ok(organizationService.add(organization));
-    }
-
-    /**
-     * @method 公司管理修改功能
-     * @param pOrgsId
-     * @param orgsName
-     * @param id
-     * @return
-     */
-    @PutMapping("/orgsUpdate")
-    public CommonResponse orgsUdt(@RequestParam String pOrgsId,@RequestParam String orgsName,@RequestParam Long id){
-
-        if (id == null){
-            return CommonResponse.errorMsg("系统错误");
-        }
-        if (pOrgsId == null || " ".equals(pOrgsId)){
-            return CommonResponse.errorMsg("系统错误");
-        }
-
-        if (orgsName == null || " ".equals(orgsName)){
-            return CommonResponse.errorTokenMsg("组织名称不能为空");
-        }
-
-        Organization organization = new Organization();
-        organization.setId(id);
-        organization.setName(orgsName);
-        organization.setPcode(pOrgsId);
-
-        return CommonResponse.ok(organizationService.update(organization));
-
-    }
 }
