d6315898613e737ecf86ac0e47dbf8f2cd3c187c
liyp <13864241798@139.com>
2018-08-30 10:17:32 +0800

    sb

--- equipment-equipment-provider/equipment-equipment-provider-api/src/main/java/com/yankuang/equipment/equipment/model/SbType.java
+++ equipment-equipment-provider/equipment-equipment-provider-api/src/main/java/com/yankuang/equipment/equipment/model/SbType.java
@@ -40,6 +40,43 @@ public class SbType implements Serializable {
 
     private Long version;
 
+
+    private String mainPara;
+
+    private String mainParaType;
+
+    private String defPara1;
+
+    private String defPara1Type;
+
+    private String defPara2;
+
+    private String defPara2Type;
+
+    private String defPara3;
+
+    private String defPara3Type;
+
+    private String defPara4;
+
+    private String defPara4Type;
+
+    private String defPara5;
+
+    private String defPara5Type;
+
+    private String defPara6;
+
+    private String defPara6Type;
+
+    private String defPara7;
+
+    private String defPara7Type;
+
+    private String defPara8;
+
+    private String defPara8Type;
+
     private List<SbType> children = new ArrayList<SbType>();
 
     public Long getId() {
@@ -154,5 +191,149 @@ public class SbType implements Serializable {
 
     public void setChildren(List<SbType> children) {
         this.children = children;
+    }
+
+    public String getMainPara() {
+        return mainPara;
+    }
+
+    public void setMainPara(String mainPara) {
+        this.mainPara = mainPara;
+    }
+
+    public String getMainParaType() {
+        return mainParaType;
+    }
+
+    public void setMainParaType(String mainParaType) {
+        this.mainParaType = mainParaType;
+    }
+
+    public String getDefPara1() {
+        return defPara1;
+    }
+
+    public void setDefPara1(String defPara1) {
+        this.defPara1 = defPara1;
+    }
+
+    public String getDefPara1Type() {
+        return defPara1Type;
+    }
+
+    public void setDefPara1Type(String defPara1Type) {
+        this.defPara1Type = defPara1Type;
+    }
+
+    public String getDefPara2() {
+        return defPara2;
+    }
+
+    public void setDefPara2(String defPara2) {
+        this.defPara2 = defPara2;
+    }
+
+    public String getDefPara2Type() {
+        return defPara2Type;
+    }
+
+    public void setDefPara2Type(String defPara2Type) {
+        this.defPara2Type = defPara2Type;
+    }
+
+    public String getDefPara3() {
+        return defPara3;
+    }
+
+    public void setDefPara3(String defPara3) {
+        this.defPara3 = defPara3;
+    }
+
+    public String getDefPara3Type() {
+        return defPara3Type;
+    }
+
+    public void setDefPara3Type(String defPara3Type) {
+        this.defPara3Type = defPara3Type;
+    }
+
+    public String getDefPara4() {
+        return defPara4;
+    }
+
+    public void setDefPara4(String defPara4) {
+        this.defPara4 = defPara4;
+    }
+
+    public String getDefPara4Type() {
+        return defPara4Type;
+    }
+
+    public void setDefPara4Type(String defPara4Type) {
+        this.defPara4Type = defPara4Type;
+    }
+
+    public String getDefPara5() {
+        return defPara5;
+    }
+
+    public void setDefPara5(String defPara5) {
+        this.defPara5 = defPara5;
+    }
+
+    public String getDefPara5Type() {
+        return defPara5Type;
+    }
+
+    public void setDefPara5Type(String defPara5Type) {
+        this.defPara5Type = defPara5Type;
+    }
+
+    public String getDefPara6() {
+        return defPara6;
+    }
+
+    public void setDefPara6(String defPara6) {
+        this.defPara6 = defPara6;
+    }
+
+    public String getDefPara6Type() {
+        return defPara6Type;
+    }
+
+    public void setDefPara6Type(String defPara6Type) {
+        this.defPara6Type = defPara6Type;
+    }
+
+    public String getDefPara7() {
+        return defPara7;
+    }
+
+    public void setDefPara7(String defPara7) {
+        this.defPara7 = defPara7;
+    }
+
+    public String getDefPara7Type() {
+        return defPara7Type;
+    }
+
+    public void setDefPara7Type(String defPara7Type) {
+        this.defPara7Type = defPara7Type;
+    }
+
+    public String getDefPara8() {
+        return defPara8;
+    }
+
+    public void setDefPara8(String defPara8) {
+        this.defPara8 = defPara8;
+    }
+
+    public String getDefPara8Type() {
+        return defPara8Type;
+    }
+
+    public void setDefPara8Type(String defPara8Type) {
+        this.defPara8Type = defPara8Type;
     }
 }
