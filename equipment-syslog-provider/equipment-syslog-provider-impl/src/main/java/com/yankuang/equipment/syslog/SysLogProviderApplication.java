9d71832b9f4195a1946f6c786a3a84fe1fdd1f89
14011 <1401165972@qq.com>
2018-10-19 14:54:35 +0800

    syslog服务删除mysql配置，添加mongo配置

--- equipment-syslog-provider/equipment-syslog-provider-impl/src/main/java/com/yankuang/equipment/syslog/SysLogProviderApplication.java
+++ equipment-syslog-provider/equipment-syslog-provider-impl/src/main/java/com/yankuang/equipment/syslog/SysLogProviderApplication.java
@@ -2,11 +2,12 @@ package com.yankuang.equipment.syslog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  *
  */
-@SpringBootApplication(scanBasePackages = "com.yankuang.equipment.syslog")
+@SpringBootApplication(scanBasePackages = "com.yankuang.equipment.syslog",exclude = DataSourceAutoConfiguration.class)
 public class SysLogProviderApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SysLogProviderApplication.class);
